6a7e02b1268870f8286802f301827cc3f92c613d
islamCodehood <islam.sayed8@gmail.com>
2024-04-24 23:54:35 +0200

    add store, and provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/not-found/not-found.page.tsx';
 import PokemonPage from './pages/pokemon/pokemon.page.tsx';
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 const router = createBrowserRouter([
 	{
@@ -22,6 +24,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
