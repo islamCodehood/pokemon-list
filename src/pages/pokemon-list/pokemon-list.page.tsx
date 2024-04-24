77140f3c96dcebfe4371e21df7bfa411c4c2cea5
islamCodehood <islam.sayed8@gmail.com>
2024-04-24 22:45:33 +0200

    Add Pokemon List page component

---
+++ src/pages/pokemon-list/pokemon-list.page.tsx
@@ -0,0 +1,9 @@
+const PokemonListPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Pokemon List</h1>
+      <p>Here is a Pokemon List!</p>
+    </div>
+  );
+}
+export default PokemonListPage
