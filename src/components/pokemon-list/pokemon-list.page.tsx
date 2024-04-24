b8d85cb9bb86d96e415e29d0bb6561a003a318e2
islamCodehood <islam.sayed8@gmail.com>
2024-04-24 23:06:59 +0200

    change file location

---
+++ src/components/pokemon-list/pokemon-list.page.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const PokemonList: React.FC = () => {
+  return (
+    <div>
+      <h1>Pokemon List</h1>
+      <p>Here is a Pokemon List!</p>
+    </div>
+  );
+}
+export default PokemonList
--- src/pages/pokemon-list/pokemon-list.page.tsx
+++
@@ -1,9 +0,0 @@
-const PokemonListPage: React.FC = () => {
-  return (
-    <div>
-      <h1>Pokemon List</h1>
-      <p>Here is a Pokemon List!</p>
-    </div>
-  );
-}
-export default PokemonListPage
