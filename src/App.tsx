a967b37e48afd1a16ad8cf386a14ac0bb5ff5bb0
islamCodehood <islam.sayed8@gmail.com>
2024-04-24 22:44:26 +0200

    Update App.tsx to include Pokemon header and main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
+
   return (
     <>
+      <div id="app">
+        <header>
+          <h1>Pokemon</h1>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </>
   )
 }
