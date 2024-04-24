396c5a206dcdb56407c8a939890455365f200a81
islamCodehood <islam.sayed8@gmail.com>
2024-04-24 22:48:00 +0200

    Add Pokemon List and Pokemon page components routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React, { Children } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/not-found/not-found.page.tsx';
+import PokemonPage from './pages/pokemon/pokemon.page.tsx';
+import PokemonListPage from './pages/pokemon-list/pokemon-list.page.tsx';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: 'pokemon/',
+				element: <PokemonListPage />,
+			},
+			{
+				path: 'pokemon/:pokemonId',
+				element: <PokemonPage />,
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
