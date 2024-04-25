3d76b53579e955840b1fc3ecd45c8d3253ff0f3b
islamCodehood <islam.sayed8@gmail.com>
2024-04-25 09:20:45 +0200

    Refactor main.tsx to simplify component structure and update Redux store configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,12 @@
-import React, { Children } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ErrorPage from './pages/not-found/not-found.page.tsx';
-import PokemonPage from './pages/pokemon/pokemon.page.tsx';
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: 'pokemon/:pokemonId',
-				element: <PokemonPage />,
-			},
-		],
-	},
-]);
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
