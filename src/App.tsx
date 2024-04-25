bbe71b2165fe9c9c40e1e133b92df64a2c8464fc
islamCodehood <islam.sayed8@gmail.com>
2024-04-25 09:20:21 +0200

    Refactor App component to include PokemonList and Pokemon components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import PokemonList from './components/pokemon-list/pokemon-list.component';
+import Pokemon from './components/pokemon/pokemon.component';
 
 function App() {
-
-  return (
-    <>
-      <div id="app">
-        <header>
-          <h1>Pokemon</h1>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </>
-  )
+  const [selectedPokemonId, setSelectedPokemonId] = useState<string>('');
+	return (
+		<>
+			<div id='app'>
+				<header >
+					<h1>Pokemon</h1>
+				</header>
+				<main>
+					<PokemonList selectPokemon={(id: string) => setSelectedPokemonId(id)} />
+					<Pokemon pokemonId={selectedPokemonId} />
+				</main>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
